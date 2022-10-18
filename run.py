79bfc80dd7bef962c43343b68556a51a4d9992af
jojo259 <95320642+jojo259@users.noreply.github.com>
2022-10-18 23:12:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -28,6 +28,7 @@ dbClient = pymongo.MongoClient(mongoConnectString)
 curDb = dbClient['hypixel']
 serversCol = curDb['servers'] # KOS functionality deprecated
 discordsCol = curDb['discords']
+discordsCol = curDb['pitdiscords']
 print('connected')
 
 botToken = os.environ['bottoken']
